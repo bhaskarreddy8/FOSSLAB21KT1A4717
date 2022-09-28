6ccd1b409a0a51112f653395584a029b98943b7a
bhaskarreddy8 <114555210+bhaskarreddy8@users.noreply.github.com>
2022-09-28 10:47:42 +0530

    Add files via upload

---
+++ s1.sh
@@ -0,0 +1,15 @@
+#this our first program,
+echo "hello wellcome to foss lab"
+ a=25
+ b=35
+ c=45
+echo "a=$a,b=$b,c=$c"
+sum=`expr $a + $b + $c`
+avg=`expr $a + $b + $c / 3`
+echo $sum,$avg 
+
+
+
+
+
+
